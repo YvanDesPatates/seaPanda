ff8ac234d28ca37fb85fcb3fff9bb839a11784db
YvanDesPatates <90316755+YvanDesPatates@users.noreply.github.com>
2022-01-23 10:29:17 +0100

    Poissons (new prefabs et regards dans bonne direction)

--- mape_code_game_jam/Assets/script/Fish.cs
+++ mape_code_game_jam/Assets/script/Fish.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     private float positionOnStart;
+    private float zAngle;
+    private Vector3 targetPos;
+    private int z;
 
-    private float yDirection;
+    public Transform cible;
 
-    // Start is called before the first frame update
     void Start()
     {
-        positionOnStart = transform.position.x;
-        yDirection = Random.Range(-1, 2);;
+        zAngle = Random.Range(-10, 10);
+        transform.Rotate(0, 0, zAngle);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // despawn when past left edge of the screen (camera)
-		if (transform.position.x < positionOnStart - 50) {
-			Destroy(gameObject);
-		}
-		// scroll based on FishSpawner static variable, speed
-		transform.Translate(-FishSpawner.speed * Time.deltaTime, FishSpawner.speed / 2 * yDirection * Time.deltaTime, 0);
-	}
+        z = Random.value < 0.55 ? 1 : -1;
+        transform.Rotate(0, 0, z);
+        Vector3 dir = cible.position - transform.position;
+        transform.Translate(dir.normalized * FishSpawner.speed * Time.deltaTime, Space.World);
+        targetPos = cible.position;
+        transform.right = cible.position - transform.position;
+        cible.position = targetPos;
+
+        if (transform.position.x < positionOnStart - 50)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
